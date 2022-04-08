43ccdcabd343b7f2cb6fc3f24bea7218244ac21d
Paul Ivey <47188956+paul-msft@users.noreply.github.com>
2022-04-08 10:07:02 +0100

    Update main.go

--- .github/actions/hello-world/main.go
+++ .github/actions/hello-world/main.go
@@ -16,7 +16,7 @@ thirdGreeting := os.Getenv("INPUT_THIRDGREETING")
 fmt.Println("Hello " + firstGreeting)
 fmt.Println("Hello " + secondGreeting)
 
-// Someimes inputs are not "required" and we can build around that
+// Sometimes inputs are not "required" and we can build around that
 if thirdGreeting != "" {
     fmt.Println("Hello " + thirdGreeting)
     }
